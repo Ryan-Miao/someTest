66a64a95227bbaf17173247f9e47f53b060b3718
chenxing12 <woshimrf@126.com>
2016-02-26 19:40:01 +0800

    多线程--同步上锁

---
+++ src/main/java/com/test/java/thread/ThreadA.java
@@ -0,0 +1,39 @@
+package com.test.java.thread;
+
+/**
+ * Created by mrf on 2016/2/26.
+ */
+public class ThreadA extends Thread {
+    private Demo demo;
+    public ThreadA(Demo demo){
+        this.demo = demo;
+    }
+    public void run(){
+        demo.getvalue();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Demo demo = new Demo();
+        for (int i = 0; i < 100; i++) {
+            new ThreadA(demo).start();
+        }
+        Thread.sleep(500);
+        System.out.println(demo.getI());
+    }
+}
+
+
+class Demo{
+    private int i = 0;
+    public synchronized int getvalue(){
+        return i++;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+}
