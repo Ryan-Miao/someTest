21a7456efa3c209c8301228a515d9d2c7b80ec7f
rmiao <v-rmiao@expedia.com>
2016-11-14 01:20:16 -0600

    add jackson date format

--- src/main/java/com/test/json/jackson/TestJackson.java
+++ src/main/java/com/test/json/jackson/TestJackson.java
@@ -1,19 +1,19 @@
 package com.test.json.jackson;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.ArrayType;
 import com.fasterxml.jackson.databind.type.CollectionType;
+import com.google.gson.Gson;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by rmiao on 9/6/2016.
@@ -62,14 +62,42 @@ public class TestJackson {
 
     }
 
+    @Test
+    public void testDateFormat() throws Exception{
+        User user  = new User("Ryan", new Date());
+        String s = mapper.writeValueAsString(user);
+        System.out.println(s);
+
+        Gson gson = new Gson();
+        String s1 = gson.toJson(user);
+        System.out.println(s1);
+
+    }
+
 }
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 class User implements Serializable {
     private static final long serialVersionUID = -5952920972581467417L;
     private String name;
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ",timezone = "GMT")
+    private Date birth;
 
     public User() {
+    }
+
+    public User(String name, Date birth) {
+        this.name = name;
+        this.birth = birth;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
     }
 
     public User(String name) {
