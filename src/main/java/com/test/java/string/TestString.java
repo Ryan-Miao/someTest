62d9d62317c4302de88f21b608f982917932af60
Ryan <woshimrf@126.com>
2017-11-25 23:06:12 +0800

    yaml dump

--- src/main/java/com/test/java/string/TestString.java
+++ src/main/java/com/test/java/string/TestString.java
@@ -7,6 +7,9 @@ import org.junit.Test;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by Ryan Miao on 9/20/17.
  */
@@ -21,7 +24,7 @@ public class TestString {
         //str.intern(): 若常量池存在，返回常量池中的对象；若常量池不存在，放入常量池，并返回this。
         //=号赋值，若常量池存在，直接返回常量池中的对象0xs1,如果常量池不存在，则放入常量池，常量池中的对象也是0xs1
         String s1 = "RyanMiao";//0xs1
-        Assert.assertTrue(s1.intern() == s1);//0xs1 == 0xs1  > true
+        assertTrue(s1.intern() == s1);//0xs1 == 0xs1  > true
     }
 
     /**
@@ -34,7 +37,7 @@ public class TestString {
         //new赋值，直接堆中创建0xs2, 常量池中All literal strings and string-valued constant expressions are interned，
         // "ab"本身就是一个字符串，并放入常量池，故intern()返回0xab
         String s2 = new String("RyanMiao");
-        Assert.assertFalse(s2.intern() == s2);//0xab == 0xs2  > false
+        assertFalse(s2.intern() == s2);//0xab == 0xs2  > false
 
     }
 
@@ -48,7 +51,7 @@ public class TestString {
     public void testConcat() throws InterruptedException {
         //没有任何字面量为"ab"暴露给编译器，所以常量池没有创建"ab"，所以，intern返回this
         String s3 = new StringBuilder("Ryan").append("Miao").toString();
-        Assert.assertTrue(s3.intern() == s3);
+        assertTrue(s3.intern() == s3);
 
     }
 
@@ -63,17 +66,17 @@ public class TestString {
         //为毛常量池存在java这个单词
         //s4 == 0xs4, intern发现常量池存在，返回0xexistjava
         String s4 = new StringBuilder("ja").append("va").toString();
-        Assert.assertFalse(s4.intern() == s4);  //0xexistjava == 0xs4  > false
+        assertFalse(s4.intern() == s4);  //0xexistjava == 0xs4  > false
 
         //int也一开始就存在于常量池中了， intern返回0xexistint
         String s5 = new StringBuilder().append("in").append("t").toString();
-        Assert.assertFalse(s5.intern()==s5); // 0xexistint == 0xs5  > false
+        assertFalse(s5.intern()==s5); // 0xexistint == 0xs5  > false
 
 
         //由于字面量"abc"加载时，已放入常量池，故s6 intern返回0xexistabc, 而s6是新建的0xs6
         String a = "abc";
         String s6 = new StringBuilder().append("ab").append("c").toString();
-        Assert.assertFalse(s6.intern() == s6);  //0xexistabc  == 0xs6 > false
+        assertFalse(s6.intern() == s6);  //0xexistabc  == 0xs6 > false
 
         Thread.sleep(1000*60*60);
 
@@ -99,5 +102,19 @@ public class TestString {
 
         Object o1 = map.get(user);
         Assert.assertNull(o1);
+    }
+
+    /**
+     * 并不会因为顺序而导致结果不同，因为在class load的时候，字面量已经加载到字符串常量池了。
+     */
+    @Test
+    public void testOrder(){
+        String a = new String("a");
+        a.intern();
+        String b = "a";
+        String c = "a";
+
+        assertFalse(a == b);
+       assertTrue(b == c);
     }
 }
--- src/main/java/com/test/yaml/SnakeYAMLTest.java
+++ src/main/java/com/test/yaml/SnakeYAMLTest.java
@@ -3,9 +3,13 @@ package com.test.yaml;
 import com.fasterxml.jackson.databind.util.JSONWrappedObject;
 import org.codehaus.jettison.json.JSONObject;
 import org.junit.Test;
+import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.Writer;
 
 /**
  * https://bitbucket.org/asomov/snakeyaml/wiki/Documentation
@@ -15,11 +19,18 @@ import java.io.InputStream;
 public class SnakeYAMLTest {
 
     @Test
-    public void testRead(){
-        Yaml yaml = new Yaml();
-        String name = "config.test.yaml";
+    public void testRead() throws IOException {
+        final DumperOptions options = new DumperOptions();
+        options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
+        options.setPrettyFlow(true);
+
+        Yaml yaml = new Yaml(options);
+        String name = "parking2.yml";
         InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream(name);
         Object load = yaml.load(resourceAsStream);
         System.out.println(load);
+
+        Writer writer = new FileWriter("D:\\workspace\\test\\someTest\\src\\main\\resources\\parking3.yml");
+        yaml.dump(load, writer);
     }
 }
