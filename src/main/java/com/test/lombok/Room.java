6250ce1fa66f14e88364eee2040ae6f2ea7fa6a2
ryan <v-rmiao@expedia.com>
2018-01-19 20:33:45 +0800

    lombok

--- src/main/java/com/test/lombok/Room.java
+++ src/main/java/com/test/lombok/Room.java
@@ -3,8 +3,11 @@ package com.test.lombok;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NonNull;
+import lombok.Singular;
+import org.junit.Assert;
 
 import java.util.Date;
+import java.util.Set;
 
 /**
  * Created by Ryan Miao on 1/18/18.
@@ -17,19 +20,19 @@ public class Room {
     private String name;
     private boolean active;
     private Date createTime;
+    @Singular
+    private Set<String> occupations;
 
     public static void main(String[] args) {
         Room room = Room.builder().active(true)
                 .name("name")
                 .id("id")
                 .createTime(new Date())
+                .occupation("1")
+                .occupation("2")
                 .build();
 
-
-        room.getCreateTime();
-        room.setActive(false);
-        room.setId("id");
-        room.getId();
+        Assert.assertEquals(2, room.getOccupations().size());
 
     }
 }
