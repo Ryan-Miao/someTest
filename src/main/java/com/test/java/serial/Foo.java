be8be7f029d51a9df5d61240f5026d0e4cb27548
Ryan <woshimrf@126.com>
2018-02-27 22:53:43 +0800

    serializable

--- src/main/java/com/test/java/serial/Foo.java
+++ src/main/java/com/test/java/serial/Foo.java
@@ -5,13 +5,17 @@ import lombok.Data;
 
 import java.io.Serializable;
 
+/**
+ * @author Ryan Miao
+ */
 @Data
 @Builder
 public class Foo implements Serializable {
 
     private static final String LOGGER = "logger";
     public static final String PUB_STATIC_FINAL = "publicStaticFinal";
-    public static String PUB_STATIC = "publicStatic";
+    private static final long serialVersionUID = 3963846241085163310L;
+    public static String PUB_STATIC;
 
     public String fa;
     private String fb;
--- src/main/java/com/test/java/serial/TestSerialize.java
+++ src/main/java/com/test/java/serial/TestSerialize.java
@@ -18,21 +18,26 @@ public class TestSerialize {
                 .tb("tb")
                 .build();
 
-        ObjectOutputStream os = new ObjectOutputStream(
-                new FileOutputStream(filename));
+        Foo.PUB_STATIC  = "test";
+
+        ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(filename));
         os.writeObject(foo);
         os.flush();
         os.close();
 
+    }
 
+    @Test
+    public void testRead() throws IOException, ClassNotFoundException {
         ObjectInputStream is = new ObjectInputStream(new FileInputStream(filename));
         Foo foo2 = (Foo) is.readObject();
         is.close();
 
-        Assert.assertEquals(foo.getFa(), foo2.getFa());
-        Assert.assertEquals(foo.getFb(), foo2.getFb());
+        Assert.assertEquals("fa", foo2.getFa());
+        Assert.assertEquals("fb", foo2.getFb());
         Assert.assertEquals(null, foo2.getTa());
         Assert.assertEquals(null, foo2.getTb());
 
+       Assert.assertNull(foo2.PUB_STATIC);
     }
 }
